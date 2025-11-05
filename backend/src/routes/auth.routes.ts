4a5fe5362b1f877ff33960b88af905624a9a1689
nguyenloc-vietq <nguyenloc@viet-q.com>
2025-11-05 17:22:58 +0700

    vietq-hrm-nestjs

--- backend/src/routes/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import  express  from 'express';
-import authControllelr from '../controllers/auth.controller';
-
-
-const router = express.Router();
-
-// 
-router.post("/login", authControllelr.login);
-export default router;
